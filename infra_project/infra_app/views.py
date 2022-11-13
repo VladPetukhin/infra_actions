f4ac1fe1ec63767716dc84354cb3e48ef85ac48a
VladPetukhin <104086673+VladPetukhin@users.noreply.github.com>
2022-11-13 17:17:55 +0300

    asd

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось но не все!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
